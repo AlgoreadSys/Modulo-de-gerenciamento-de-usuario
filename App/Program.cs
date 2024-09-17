d42846447a9f815287c8e05a0b0b1c7b89f08205
Jhonnathan Jhonny <104373339+Jhonnathan-Jhonny@users.noreply.github.com>
2024-09-17 09:37:45 -0300

    xp-102
    função para usuário atualizar as informações necessárias.
    Foi realizado as verificações necessárias

--- App/Program.cs
+++ App/Program.cs
@@ -14,8 +14,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 // Registra o Supabase.Client como Singleton
-var url = "https://eabruaevckpvepqqqmfv.supabase.co";
-var key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVhYnJ1YWV2Y2twdmVwcXFxbWZ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU4MjczOTgsImV4cCI6MjA0MTQwMzM5OH0.Eczsx_ij7fybFr3cZFBP0jtmmxbZMZYwP-FeuBjciyE";
+const string url = "https://eabruaevckpvepqqqmfv.supabase.co";
+const string key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVhYnJ1YWV2Y2twdmVwcXFxbWZ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU4MjczOTgsImV4cCI6MjA0MTQwMzM5OH0.Eczsx_ij7fybFr3cZFBP0jtmmxbZMZYwP-FeuBjciyE";
 var options = new SupabaseOptions
 {
     AutoRefreshToken = true,
@@ -77,6 +77,48 @@ app.MapPost("/UserCreat/{id:long}", async (long id, User user, Supabase.Client c
     }
 
 });
+
+app.MapPut("/UserModify/{id:long}", async (long id,User user, Supabase.Client client, HttpContext httpContext) =>
+{
+    // Verifica se o usuário está logado
+    if (!await AuthHelper.IsUserLoggedIn(httpContext, client))
+    {
+        return Results.Unauthorized();
+    }
+    
+    var userResponseInfo = await client
+        .From<User>()
+        .Where(userBd => userBd.Id == id)
+        .Get();
+
+    var existingUser = userResponseInfo.Models.FirstOrDefault();
+    if (existingUser == null)
+    {
+        return Results.NotFound("User not found.");
+    }
+    
+    try
+    {
+        // Atualiza o usuário no banco de dados
+        var response = await client
+            .From<User>()
+            .Where(userBd => userBd.Id == id)
+            .Set(userSave => userSave.Name!,user.Name ?? existingUser.Name)
+            .Set(userSave => userSave.ProfileName!, user.ProfileName ?? existingUser.ProfileName)
+            .Set(userSave => userSave.BirthData!, user.BirthData ?? existingUser.BirthData)
+            .Set(userSave => userSave.Email!, user.Email ?? existingUser.Email)
+            .Update();
+        // Verifica se algum usuário foi atualizado
+        var updatedUser = response.Models.FirstOrDefault();
+        return updatedUser == null ? Results.NotFound("Usuário não encontrado.") : Results.Ok(new { Id = updatedUser.Id });
+    }
+    catch (Exception ex)
+    {
+        // Trata erros e retorna uma mensagem de erro
+        return Results.Problem($"Erro ao atualizar o usuário: {ex.Message}");
+    }
+});
+
 
 app.UseHttpsRedirection();
 app.Run();
